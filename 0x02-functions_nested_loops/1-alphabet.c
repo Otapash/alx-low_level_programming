faa38489e8326b56c27b59e9bff9487084d9b4f4
otapash <otapash@gmail.com>
2023-02-21 14:07:09 -0800

    Task 1- print alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets
+ *
+ * Description: a function that prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	char lara = 'a';
+
+	while (lara = 'z')
+	{
+		_putchar (lara);
+		lara++;
+	}
+	_putchar ('\n');
+}	
